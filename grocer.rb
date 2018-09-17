5f4dbf3523d92780946909099c0fa41198b00b12
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-17 05:19:10 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,32 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = cart.reduce(&:merge)              # => {"TEMPEH"=>{:price=>3.0, :clearance=>true}, "PEANUTBUTTER"=>{:price=>3.0, :clearance=>true}, "ALMONDS"=>{:price=>9.0, :clearance=>false}}, {"AVOCADO"=>{:price=>3.0, :clearance=>true}, "KALE"=>{:price=>3.0, :clearance=>false}}
+  new_cart.each do |k, v|                      # => {"TEMPEH"=>{:price=>3.0, :clearance=>true}, "PEANUTBUTTER"=>{:price=>3.0, :clearance=>true}, "ALMONDS"=>{:price=>9.0, :clearance=>false}}, {"AVOCADO"=>{:price=>3.0, :clearance=>true}, "KALE"=>{:price=>3.0, :clearance=>false}}
+    v[:count] = cart.count {|i| i == {k => v}}  # => 1, 1, 1, 2, 1
+  end
 end
 
+# @param cart [Array]
+# @param coupons [Hash]
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    item_name = coupon[:item]
+    cart_item = cart[item_name]
+    next unless cart_item
+    cart_item[:count] -= coupon[:num]
+    if cart_item[:count] <= 0
+      cart.delete(cart_item)
+    end
+
+    # the output format doesn't really acount for multiple coupons for the
+    # same item with different values
+    coupon_item = (cart["#{item_name} W/COUPON"] ||= {
+      price: coupon[:cost],
+      clearance: cart_item[:clearance],
+      count: 0
+    })
+    coupon_item[:count] += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
